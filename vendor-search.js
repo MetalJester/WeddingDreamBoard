44ab04ca9a9c850c06c46ec60b850b9559cf1991
rkpeltier <rkpeltier@gmail.com>
2019-02-09 15:13:19 -0800

    the map is now working

--- vendor-search.js
+++ vendor-search.js
@@ -2,33 +2,42 @@
 var map;
 var infoWindow;
 
-var apiKey = "AIzaSyAxugqYRJAdsSejK6O4BeNVaI3KQ4KnqkA"
 
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map')), {
-        center: {lat: 38.575764, lng: -121.478851},
-        zoom: 12
-      }
-      infoWindow = new google.maps.InfoWindow;
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 38.575764, lng: -121.478851},
+    zoom: 14
+  });
+  infoWindow = new google.maps.InfoWindow;
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent('Location found.');
-          infoWindow.open(map);
-          map.setCenter(pos);
-        }, function() {
-          handleLocationError(true, infoWindow, map.getCenter());
-        });
-      } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-      }
-    }
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
 
     //To add markers for vendors to the map
     function newMarker() {
